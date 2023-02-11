36287212343d517403b7eac2ba41a0f27be08568
Aatif Ali <aatifwarraich10@gmail.com>
2023-02-11 14:46:13 +0500

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import User from "./userProfile/User";
-import Home from "./components/Home";
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
           <Route path="/" element={<User />}></Route>
         </Routes>
       </BrowserRouter>
